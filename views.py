77e16711150c90f5e53cec4398048ecd03554602
antholo <blooker@gmail.com>
2014-12-30 15:22:43 -0600

    Comments added

--- views.py
+++ views.py
@@ -10,6 +10,11 @@ import os
 import auth2 as d2lauth
 
 
+##########
+# config #
+##########
+
+
 app = Flask(__name__)
 app.config.from_pyfile('app_config.cfg')
 mail = Mail(app)
@@ -18,6 +23,12 @@ CsrfProtect(app)
 
 appContext = d2lauth.fashion_app_context(app_id=app.config['APP_ID'],
                                          app_key=app.config['APP_KEY'])
+
+
+
+############
+# wrappers #
+############
 
 
 def login_required(test):
@@ -31,9 +42,17 @@ def login_required(test):
     return wrap
 
 
+##########
+# routes #
+##########
+
+
 @app.route('/logout')
 @login_required
 def logout():
+    '''
+    Clears stored session information.
+    '''
     session.clear()
     return redirect(app.config['REDIRECT_AFTER_LOGOUT'])
 
@@ -42,8 +61,9 @@ def logout():
 @app.route('/login')
 def login():
     '''
-    Checks if user context is stored in session and redirects to authorization handler if it is.
-    If not, renders login template with link to D2L login and callback route to authorization handler.
+    Checks if user context is stored in session and redirects to authorization
+    handler if it is. If not, renders login template with link to D2L login and
+    callback route to authorization handler.
     '''
     if 'userContext' in session:
         return redirect(url_for(app.config['AUTH_ROUTE']))
@@ -57,21 +77,22 @@ def login():
 
 @app.route(app.config['AUTH_ROUTE'])
 def auth_handler():
+    '''
+    Creates and stores user context and details.
+    '''
     uc = appContext.create_user_context(
         result_uri=request.url, 
         host=app.config['LMS_HOST'],
         encrypt_requests=app.config['ENCRYPT_REQUESTS'])
-    my_url = uc.create_authenticated_url('/d2l/api/lp/{0}/users/whoami'.format(app.config['VER']))
+    my_url = uc.create_authenticated_url(
+        '/d2l/api/lp/{0}/users/whoami'.format(app.config['VER']))
     r = requests.get(my_url)
-
-    #print('WHOAMI', r.json())
 
     session['firstName'] = r.json()['FirstName']
     session['lastName'] = r.json()['LastName']
     session['userId'] = r.json()['Identifier']
 
     """PRODUCTION: UNCOMMENT FOLLOWING LINE AND DELETE THE ONE AFTER THAT"""
-    
     #session['uniqueName'] = r.json()['UniqueName']
     session['uniqueName'] = 'lookerb'
 
@@ -89,6 +110,9 @@ def auth_handler():
 @app.route('/semester', methods=['GET', 'POST'])
 @login_required
 def select_semester():
+    '''
+    Generates form with semester options for user to select.
+    '''
     error = None
     form = SelectSemesterForm(request.form)
     if 'semCode' in session:
@@ -96,14 +120,15 @@ def select_semester():
     if 'coursesToCombine' in session:
         session.pop('coursesToCombine')
     if request.method == 'POST':
-        if form.is_submitted():
+        if form.validate_on_submit():
             semCode = get_semester(form.semester.data, form.year.data)
             session['semCode'] = semCode
 
             try:
                 courseDict = session['courseDict'][semCode]
             except KeyError:
-                error = "No courses are listed with you enrolled as an instructor for that semester."
+                error = "No courses are listed with you enrolled" + \
+                    "as an instructor for the selected semester."
                 return render_template("semester.html", form=form, error=error)
         return redirect(url_for('enrollment_handler'))
     else:
@@ -113,22 +138,25 @@ def select_semester():
 @app.route('/enrollments', methods=['GET', 'POST'])
 @login_required
 def enrollment_handler():
+    '''
+    Generates forms with courses to select for combining and to add more
+    courses to that list. 
+    '''
     error = None
     uc = appContext.create_user_context(
             d2l_user_context_props_dict=session['userContext'])
     courseDict = session['courseDict'][session['semCode']]
     form = SelectCoursesForm(request.form, prefix="form")
-    form.courseIds.choices, form.baseCourse.choices = get_courseId_choices(courseDict), get_baseCourse_choices(courseDict)
+    form.courseIds.choices = get_courseId_choices(courseDict)
+    form.baseCourse.choices = get_baseCourse_choices(courseDict)
     add_form = AdditionalCourseForm(request.form, prefix="add_form")
     if request.method == 'POST':
         if form.is_submitted():
             if request.form['btn'] == 'Add Class' and add_form.validate_on_submit():
-                #add_form.validate()
-                print add_form.errors
                 code = make_code(add_form)
                 courseToAdd = get_course(uc, code)
                 if not courseToAdd:
-                    error = "Could not find a class for the information you entered. Please check course details and try again."
+                    error = "Please check course details and try again."
                     return render_template("enrollments.html", form=form, add_form=add_form, error=error)
                 session['courseDict'][session['semCode']] = update_course_dict(courseDict,
                     courseToAdd['Identifier'],
@@ -136,10 +164,7 @@ def enrollment_handler():
                     code)
                 return redirect(url_for('enrollment_handler'))
             elif request.form['btn'] == 'Add Class':
-                print("RAW", add_form.errors)
                 error = add_form.errors.values()[0][0]
-                #error = [e[0] for e in error]
-                print("ERROR", error)
                 return render_template("enrollments.html", form=form, add_form=add_form, error=error)
             elif request.form['btn'] == 'Submit Request':
                 courseIds = form.courseIds.data
@@ -155,6 +180,7 @@ def enrollment_handler():
                     return render_template("enrollments.html", form=form, add_form=add_form, error=error)
                 session['baseCourse'], session['coursesToCombine'] = baseCourse, coursesToCombine
                 if baseCourse not in coursesToCombine:
+                    coursesToCombine.append(baseCourse)
                     return render_template("check.html", coursesToCombine=coursesToCombine, baseCourse=baseCourse)
                 else:
                     return redirect(url_for('confirm_selections'))
@@ -168,30 +194,60 @@ def enrollment_handler():
 @app.route('/confirmation')
 @login_required
 def confirm_selections():
+    '''
+    Generates confirmation page and sends confirmation emails.
+    '''
     msg = Message(subject='Course Combine Confirmation',
-        recipients=[app.config['MAIL_DEFAULT_SENDER'], session['uniqueName'] + app.config['EMAIL_DOMAIN']])
-    msg.body = generate_msg_text(session['firstName'], session['lastName'], session['coursesToCombine'], session['baseCourse'])
-    msg.html = generate_msg_html(session['firstName'], session['lastName'], session['coursesToCombine'], session['baseCourse'])
+        recipients=[app.config['MAIL_DEFAULT_SENDER'],
+        session['uniqueName'] + app.config['EMAIL_DOMAIN']])
+    msg.body = generate_msg_text(session['firstName'],
+        session['lastName'],
+        session['coursesToCombine'],
+        session['baseCourse'])
+    msg.html = generate_msg_html(session['firstName'],
+        session['lastName'],
+        session['coursesToCombine'],
+        session['baseCourse'])
     mail.send(msg)
     return render_template("confirmation.html", coursesToCombine=session['coursesToCombine'], baseCourse=session['baseCourse'])
 
 
+###########
+# helpers #
+###########
+
+
 def generate_msg_text(firstName, lastName, coursesToCombine, baseCourse):
-    opening ="Hello {0} {1},\nYou have asked to have the following courses combined into {2}, {3}:\n\nCourse Name\t(Course Id)\n".format(firstName, lastName, baseCourse['parsed'], baseCourse['name'])
-    courseTable = "\n".join("{!s}\t({!s})".format(course['name'], course['code']) for course in coursesToCombine)
-    closing = "\nIf this is incorrect, please contact our D2L site administrator at " + app.config['EMAIL_SITE_ADMIN'] + "."
+    '''
+    Generates confirmation email message text.
+    '''
+    opening ="Hello {0} {1},\nYou have asked to have the following courses" + \
+        "combined into {2}, {3}:\n\nCourse Name\t(Course Id)\n".format(
+        firstName, lastName, baseCourse['parsed'], baseCourse['name'])
+    courseTable = "\n".join("{!s}\t({!s})".format(course['name'],
+        course['code']) for course in coursesToCombine)
+    closing = "\nIf this is incorrect, please contact our D2L site" + \
+        "administrator at " + app.config['EMAIL_SITE_ADMIN'] + "."
     msg_body = opening + courseTable + closing
     return msg_body
 
 
 def generate_msg_html(firstName, lastName, coursesToCombine, baseCourse):
-    opening ="<p>Hello {0} {1},</p><p>You have asked to have the following courses combined into {2}, {3}:</p>".format(firstName, lastName, baseCourse['parsed'], baseCourse['name'])
+    '''
+    Generates confirmation email message in HTML.
+    '''
+    opening ="<p>Hello {0} {1},</p><p>You have asked to have the following" + \
+        "courses combined into {2}, {3}:</p>".format(
+        firstName, lastName, baseCourse['parsed'], baseCourse['name'])
     tableHead = "<table><thead><tr><th>Course Name</th><th>(Course Id)</th></thead>"
-    courseTable = "".join("<tr><td>{!s}</td><td>({!s})</td></tr>".format(course['name'], course['code']) for course in coursesToCombine)
+    courseTable = "".join("<tr><td>{!s}</td><td>({!s})</td></tr>".format(
+        course['name'], course['code']) for course in coursesToCombine)
     tableClose = "</table>"
-    closing = "<p>If this is incorrect, please contact our D2L site administrator at " + app.config['EMAIL_SITE_ADMIN'] + ".</p>"
+    closing = "<p>If this is incorrect, please contact our D2L site" + \
+        "administrator at " + app.config['EMAIL_SITE_ADMIN'] + ".</p>"
     msg_html = opening + tableHead + courseTable + tableClose + closing
     return msg_html
+
 
 def get_semester(semester, year):
     '''
@@ -199,15 +255,15 @@ def get_semester(semester, year):
     '''
     if semester == 'Fall':
         precedingDigits = year
-        finalDigit = '0'
+        finalDigit = app.config['FALL']
     if semester == 'Spring':
         precedingDigits = str(int(year) - 1)
-        finalDigit = '5'
+        finalDigit = app.config['SPRING']
     if semester == 'Summer':
         precedingDigits = str(int(year) - 1)
-        finalDigit = '8'
+        finalDigit = app.config['SUMMER']
     semesterCode = precedingDigits + finalDigit
-    if len(semesterCode) < 4:
+    while len(semesterCode) < 4:
         semesterCode = '0' + semesterCode
     return semesterCode
 
@@ -217,14 +273,13 @@ def get_courses(uc):
     Creates dictionary of lists of courses keyed by semester code and stores 
     it in session for easy access post-creation.
     '''
-    myUrl = uc.create_authenticated_url('/d2l/api/lp/{0}/enrollments/users/{1}/orgUnits/'.format(app.config['VER'], session['userId']))
+    myUrl = uc.create_authenticated_url(
+        '/d2l/api/lp/{0}/enrollments/users/{1}/orgUnits/'.format(
+        app.config['VER'], session['userId']))
     kwargs = {'params': {}}
     kwargs['params'].update({'roleId':app.config['ROLE_ID']})
     kwargs['params'].update({'orgUnitTypeId': app.config['ORG_UNIT_TYPE_ID']})
     r = requests.get(myUrl, **kwargs)
-
-    #print(r.json()['Items'])
-
     courseDict = {}
     end = False
     while end == False:
@@ -233,11 +288,6 @@ def get_courses(uc):
             if semCode.isdigit():
                 if semCode not in courseDict:
                     courseDict[semCode] = []
-                #courseDict[semCode].append({'courseId': course['OrgUnit']['Id'], 'name': course['OrgUnit']['Name']})
-                """courseDict[semCode].append({'courseId': course['OrgUnit']['Id'],
-                    'name': course['OrgUnit']['Name'],
-                    'code': course['OrgUnit']['Code'],
-                    'parsed': parse_code(course['OrgUnit']['Code'])})"""
                 courseDict[semCode] = update_course_dict(courseDict[semCode],
                     course['OrgUnit']['Id'],
                     course['OrgUnit']['Name'],
@@ -252,10 +302,20 @@ def get_courses(uc):
 
 
 def update_course_dict(courseDict, courseId, name, code):
-    courseDict.append({u'courseId': int(courseId), u'name': name, u'code': code, u'parsed': parse_code(code)})
+    '''
+    Adds a course's info to the courseDict.
+    '''
+    courseDict.append({u'courseId': int(courseId),
+        u'name': name,
+        u'code': code,
+        u'parsed': parse_code(code)})
     return courseDict
 
+
 def get_course(uc, code):
+    '''
+    Gets course information for supplied code from D2L.
+    '''
     myUrl = uc.create_authenticated_url('/d2l/api/lp/{0}/orgstructure/'.format(app.config['VER']))
     kwargs = {'params': {}}
     kwargs['params'].update({'orgUnitCode': code})
@@ -266,31 +326,28 @@ def get_course(uc, code):
     except IndexError:
         return False
 
-def add_course(courseId):
-    myUrl = uc.create_authenticated_url('/d2l/api/lp/{0}/orgstructure/{1}'.format(app.config['VER'], courseId))
-    r = requests.get(myUrl, **kwargs)
-    if r.json():
-        response = r.json()
-        #print r.json()
-        courseInfo = {'courseId': courseId,
-            'name': response['Name'],
-            'code': response['Code'],
-            'parsed': parse_code(response['Code'])}
-        return courseInfo
-    else:
-        print r.reason()
 
 def parse_code(code):
+    '''
+    Breaks up code into more readable version to present to user.
+    '''
     parsed = code.split("_")
     return parsed[3] + " " + parsed[4] + " " + parsed[5]
 
 
 def get_courseId_choices(courseDict):
+    '''
+    Pulls elements from courseDict to use in form choices.
+    '''
     return [(course['courseId'], 
         course['name'] + ", " + course['parsed']) for course in courseDict]
 
 
 def get_baseCourse_choices(courseDict):
+    '''
+    Pulls elements from courseDict to use in baseCourse choices, with markup to
+    make choices linkable.
+    '''
     linkPrefix = "<a target=\"_blank\" href='http://" + \
         app.config['LMS_HOST'] + \
         "/d2l/lp/manageCourses/course_offering_info_viewedit.d2l?ou="
@@ -306,6 +363,9 @@ def get_baseCourse_choices(courseDict):
 
 
 def make_code(add_form):
+    '''
+    Creates code from the elements submitted in add form.
+    '''
     return '_'.join(('UWOSH',
         session['semCode'],
         add_form.sessionLength.data,
@@ -316,6 +376,9 @@ def make_code(add_form):
 
 
 def update_base_course(baseCourse, baseCourseData, courseDict):
+    '''
+    Populates baseCourse dictionary with user-selected baseCourse data.
+    '''
     baseCourseId = int(baseCourseData)
     for course in courseDict:
         if course['courseId'] == baseCourseId:
